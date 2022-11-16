d8bb17b14168fb874461dc01518586e1749a5dea
jiangxb98 <jiangguangfeng@cowarobot.com>
2022-11-16 05:08:56 +0000

    upload

---
+++ mmdetection3d/start_dataset_learn/configs/_base_/schedules/cyclic_20e.py
@@ -0,0 +1,17 @@
+optimizer = dict(type='AdamW', lr=1e-4, betas=(0.95, 0.99), weight_decay=0.01)
+optimizer_config = dict(grad_clip=dict(max_norm=10, norm_type=2))
+lr_config = dict(
+    policy='cyclic',
+    target_ratio=(10, 1e-4),
+    cyclic_times=1,
+    step_ratio_up=0.4,
+)
+momentum_config = dict(
+    policy='cyclic',
+    target_ratio=(0.85 / 0.95, 1),
+    cyclic_times=1,
+    step_ratio_up=0.4,
+)
+
+# runtime settings
+runner = dict(type='EpochBasedRunner', max_epochs=20)
